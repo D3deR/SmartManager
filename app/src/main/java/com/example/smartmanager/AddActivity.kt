9e5d85aeeaf40ddfc107b83d4098565b070b0b70
munte <munteanu.paul27@yahoo.com>
2020-11-30 19:29:39 +0200

    Date and time picker added.

--- app/src/main/java/com/example/smartmanager/AddActivity.kt
+++ app/src/main/java/com/example/smartmanager/AddActivity.kt
@@ -1,29 +1,62 @@
 package com.example.smartmanager
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
+import androidx.annotation.RequiresApi
 import com.example.smartmanager.model.Activity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import java.text.SimpleDateFormat
+import java.util.*
+import javax.xml.datatype.DatatypeConstants.MONTHS
 
 class AddActivity : AppCompatActivity() {
 
     val firebaseUser = FirebaseAuth.getInstance()
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+
+        val timeBtn  = findViewById<Button>(R.id.timeBtn)
+        val dateBtn = findViewById<Button>(R.id.dateBtn)
+        val timeView = findViewById<TextView>(R.id.editTextTime)
+        val dateView = findViewById<TextView>(R.id.editTextDate)
+        timeBtn.setOnClickListener{
+                val cal = Calendar.getInstance()
+                val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                    cal.set(Calendar.HOUR_OF_DAY, hour)
+                    cal.set(Calendar.MINUTE, minute)
+                    timeView.text = SimpleDateFormat("HH:mm").format(cal.time)
+                }
+                TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+            }
+
+        dateBtn.setOnClickListener{
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+
+                dateView.text="$dayOfMonth/$monthOfYear/$year"
+            }, year, month, day)
+            dpd.show()
+        }
 
         val uploadBtn = findViewById<Button>(R.id.buttonUploadActivity) as Button
         uploadBtn.setOnClickListener{
             val activityName : EditText = findViewById(R.id.editTextActivityName)
             val activityDescription : EditText = findViewById(R.id.editTextActivityDescription)
-            val activityStartTime : EditText = findViewById(R.id.editTextTime)
-            val activityDate : EditText = findViewById(R.id.editTextDate)
+            val activityStartTime : TextView = findViewById(R.id.editTextTime)
+            val activityDate : TextView = findViewById(R.id.editTextDate)
             val activityColor : EditText = findViewById(R.id.editTextColor)
             val reminder : CheckBox = findViewById(R.id.checkBoxReminder)
 
